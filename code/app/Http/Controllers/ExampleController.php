5f838304e67e562abfb92d3236f26463426a0b88
Ася Скульская <savi@m18.ru>
2021-10-03 22:59:55 +0300

    redis

--- code/app/Http/Controllers/ExampleController.php
+++ code/app/Http/Controllers/ExampleController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+use function Sodium\add;
+
 class ExampleController extends Controller
 {
     /**
@@ -25,5 +28,16 @@ class ExampleController extends Controller
     public function getExample(): string
     {
         return 'example'.time();
+    }
+    public function testRedis()
+    {
+        if (Cache::has('test_redis1')) {
+            echo 'Test redis key was set on ' . date('Y-m-d H:i:s', (int)Cache::get('test_redis1'));
+        }
+        else
+        {
+            echo 'Redis is empty, setting a key... reload page.';
+            Cache::add( 'test_redis1', time(), 60 * 2 );
+        }
     }
 }
